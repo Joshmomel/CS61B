cc33fda42db32172f4c68871206e6240cb0d4086
boxianmo <123>
2018-11-25 10:38:22 +1100

    change leap year alg

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -15,10 +15,10 @@ public class LeapYear {
     }
 
     private static boolean isLeapYear(int year) {
-        if(year % 4 == 0){
+        if (year % 400 == 0){
             return true;
         }
-        if(year % 400 == 0){
+        if (year % 4 == 0 && year % 100 != 0){
             return true;
         }
         return false;
