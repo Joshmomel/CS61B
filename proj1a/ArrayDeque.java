d47a904f4b07d35c7f3efa5336c3937fe5d1e538
boxianmo <123>
2018-12-01 11:33:31 +1100

    my

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,187 +1,188 @@
 
-public class ArrayDeque<T>  {
-    private int size;
+public class ArrayDeque <T> {
     private T[] items;
     private int nextFirst;
     private int nextLast;
-    private int first;
-    private final int FINAL_LENGTH = 8;
+    private int size;
 
-    /*
-    Create an empty list
-     */
-    public ArrayDeque() {
-        items = (T[]) new Object[8];
-        nextFirst = (items.length - size) / 2;
-        nextLast = nextFirst + 1;
-        first = nextFirst;
+    private static final int INITIAL_CAPACITY = 8;
+    private static final int RFACTOR = 2;
+
+    public ArrayDeque(){
+        items = (T[]) new Object[INITIAL_CAPACITY];
+        nextFirst = 0;
+        nextLast = 1;
+        size = 0;
     }
 
-    /*
-    Resizes the underlying array to the target capacity.
-     */
-    private void resize(int capacity) {
+    private void calculateFront(){
+        nextFirst = (nextFirst + items.length - 1) % (items.length);
+    }
+
+    private void calculateLast(){
+        nextLast = (nextLast + 1) % items.length;
+    }
+
+    private int plusOne(int index){
+        return (index + 1) % items.length;
+    }
+
+    private int minusOne(int index){
+        return (index + items.length -1) % items.length;
+    }
+
+    private boolean isFull(){
+        return items.length == size;
+    }
+
+    private void extend(){
+        reSize(size, RFACTOR * size);
+    }
+
+    private void shrink(){
+        reSize(items.length, items.length / RFACTOR);
+    }
+
+    private boolean checkLarger(int nextFirst, int nextLast){
+        return nextFirst > nextLast;
+    }
+
+    private void reSize(int size, int capacity){
         T[] a = (T[]) new Object[capacity];
-        int tempLength = items.length - first;
-        System.arraycopy(items, first, a, (a.length / 2) - 1, tempLength);
-        if (tempLength != items.length) {
-            System.arraycopy(items, 0, a, ((a.length / 2) - 1) + tempLength, first);
+
+        if (capacity < size){
+            int start = Math.min(nextFirst, nextLast);
+            for (int i = 0; i < capacity; i++) {
+                a[i] = items[start];
+                start ++;
+            }
+            if (checkLarger(nextFirst,nextLast)){
+                nextLast = 0;
+                nextFirst = capacity / 2;
+            }else {
+                nextFirst = 0;
+                nextLast = capacity / 2;
+            }
+        }else {
+            
         }
         items = a;
-        nextFirst = minusOne((a.length / 2) - 1);
-        first = plusOne(nextFirst);
-        nextLast = plusOne(nextFirst + size);
-    }
-
-    /*
-    Resizes the underlying array to down to the target capacity.
-     */
-    private void resizeDown(int capacity) {
-        T[] a = (T[]) new Object[capacity];
-        if (capacity < FINAL_LENGTH) {
-            return;
-        }
-        if (first + size > items.length) {
-            int left = (first + size) - items.length;
-            int tempLength = items.length - first;
-            System.arraycopy(items, first, a, (a.length / 2) - 1, tempLength);
-            System.arraycopy(items, nextLast - 1, a, ((a.length / 2) - 1) + tempLength, left);
-        } else {
-            System.arraycopy(items, first, a, (a.length / 2) - 1, size);
-        }
-        items = a;
-        nextFirst = minusOne((a.length / 2) - 1);
-        first = plusOne(nextFirst);
-        nextLast = plusOne(nextFirst + size);
+        System.out.println();
 
     }
 
-
-    /*
-    Checks whether the array is full and needs to be resized.
-     */
-    private boolean isFull() {
-        return size == items.length;
-    }
-
-    /*
-    Adds an item of type T to the front of the deque.
-     */
-    public void addFirst(T item) {
-        if (isFull()) {
-            resize(size * 2);
+    public void addFirst(T item){
+        if (isFull()){
+            extend();
         }
 
-        first = nextFirst;
         items[nextFirst] = item;
-        nextFirst = minusOne(nextFirst);
-        size += 1;
+        calculateFront();
+        size ++;
     }
 
-    /*
-    Adds an item of type T to the back of the deque.
-     */
-    public void addLast(T item) {
-        if (isFull()) {
-            resize(size * 2);
+    public void addLast(T item){
+        if (isFull()){
+            extend();
         }
-
         items[nextLast] = item;
-        if (items[first] == null) {
-            first = nextLast;
-        }
-        nextLast = plusOne(nextLast);
-        size += 1;
+        calculateLast();
+        size ++;
     }
 
-    private void checkResize() {
-        if ((double) size / items.length < 0.25) {
-            resizeDown(items.length / 2);
-        }
-    }
-
-    /*
-    Removes and returns the item at the back of the deque. If no such item exists, returns null.
-     */
-    public T removeLast() {
-        if (isEmpty()) {
-            return null;
-        }
-        nextLast = minusOne(nextLast);
-        T item = items[nextLast];
-        items[nextLast] = null;
-        size -= 1;
-        checkResize();
-        return item;
-    }
-
-    public T removeFirst() {
-        if (isEmpty()) {
-            return null;
-        }
-
-        T item = items[first];
-        items[first] = null;
-        first = plusOne(first);
-        nextFirst = plusOne(nextFirst);
-        size -= 1;
-        checkResize();
-        return item;
-    }
-
-    /*
-    Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
-    If no such item exists, returns null.
-     */
-    public T get(int index) {
-        if (index >= size) {
-            return null;
-        } else if (first + index < items.length) {
-            return items[first + index];
-        } else {
-            return items[first + index - items.length];
-        }
-    }
-
-    /*
-    Returns the number of items in the deque.
-     */
-    public int size() {
-        return size;
-    }
-
-    /*
-    Returns x - 1 for any given integer x.
-    Used primarily for cleanliness.
-     */
-    private int minusOne(int x) {
-        if (x == 0) {
-            return items.length - 1;
-        }
-        return x - 1;
-    }
-
-    private int plusOne(int x) {
-        if (x == items.length - 1) {
-            return 0;
-        }
-        return x + 1;
-    }
-
-    /*
-    Returns true if deque is empty, false otherwise.
-     */
-    public boolean isEmpty() {
+    public boolean isEmpty(){
         return size == 0;
     }
 
-    /*
-    Prints the items in the deque from first to last, separated by a space.
-     */
-    public void printDeque() {
-        for (int i = 0; i < size; i++) {
-            System.out.print(get(i) + " ");
+    public int size(){
+        return size;
+    }
+
+    private boolean isSparse(){
+        return items.length >= 16 && size() < items.length / 4;
+    }
+
+    public T removeFirst(){
+        if (isSparse()){
+            shrink();
         }
+        nextFirst = plusOne(nextFirst);
+        T returnItem = items[nextFirst];
+        items[nextFirst] = null;
+        size --;
+        return returnItem;
+    }
+
+    public T removeLast(){
+        if (isSparse()){
+            shrink();
+        }
+        nextLast = minusOne(nextLast);
+        T returnItem = items[nextLast];
+        items[nextLast] = null;
+        size --;
+        return returnItem;
+    }
+
+
+    public void printDeque() {
+        for (int i = 0; i < items.length; i++) {
+            T item = get(i);
+            if (item == null){
+                System.out.println();
+                return;
+            }
+            System.out.print(item);
+            System.out.print(" ");
+        }
+    }
+
+    public T get(int index){
+        return items[(nextFirst + 1 + index) % items.length ];
+    }
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        arrayDeque.addFirst(1);
+//        arrayDeque.addFirst(2);
+//        arrayDeque.addFirst(3);
+//        arrayDeque.addFirst(4);
+//        arrayDeque.addFirst(5);
+        arrayDeque.addLast(-1);
+        arrayDeque.addLast(-2);
+        arrayDeque.addLast(-3);
+        arrayDeque.addLast(-4);
+        arrayDeque.addLast(-5);
+        arrayDeque.addLast(-6);
+        arrayDeque.addLast(-7);
+        arrayDeque.addLast(-8);
+        arrayDeque.addLast(-9);
+        arrayDeque.addLast(-10);
+        arrayDeque.addLast(-11);
+        arrayDeque.addLast(-12);
+        arrayDeque.addLast(-13);
+        arrayDeque.addLast(-14);
+        arrayDeque.addLast(-15);
+
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+
+
+        arrayDeque.removeFirst();
+
         System.out.println();
     }
+
+
+
 }
