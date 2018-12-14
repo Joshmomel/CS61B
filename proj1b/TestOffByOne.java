bef2adedf1a550e9b0d2636bd08a56fd88e4f723
Bok <heybokk@gmail.com>
2018-12-14 18:06:45 +1100

    update test 2

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -18,7 +18,8 @@ public class TestOffByOne {
         assertTrue(offByOne.equalChars('a', 'b'));
         assertFalse(offByOne.equalChars('a', 'c'));
         assertFalse(offByOne.equalChars('a', 'A'));
-
+        assertFalse(offByOne.equalChars('Z', 'a'));
+        
         assertTrue(offByOne.equalChars('(', ')'));
         assertTrue(offByOne.equalChars('=', '>'));
         assertTrue(offByOne.equalChars('@', 'A'));
