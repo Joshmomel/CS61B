7c146f295c77d418ca79825c80e951ee565862fe
Bok <heybokk@gmail.com>
2018-12-29 11:58:48 +1100

    change alg

--- hw2/hw2/Direction.java
+++ hw2/hw2/Direction.java
@@ -1,4 +1,19 @@
 package hw2;
 
 public class Direction {
+    public Position theNum;
+    public Position top;
+    public Position bottom;
+    public Position left;
+    public Position right;
+
+    public Direction(Position theNum) {
+        this.theNum = theNum;
+        this.top = new Position(theNum.x, theNum.y + 1);
+        this.bottom = new Position(theNum.x, theNum.y -1);
+        this.left = new Position(theNum.x - 1, theNum.y);
+        this.right = new Position(theNum.x + 1,theNum.y);
+    }
+
+
 }
--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,14 +2,14 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation implements PercolationInterface{
+public class Percolation implements PercolationInterface {
     public boolean[][] grid;
     public int topParent;
     public int bottomParent;
     public WeightedQuickUnionUF weightedQuickUnionUF;
     public int openSiteNum = 0;
 
-    public Percolation(int N){
+    public Percolation(int N) {
         weightedQuickUnionUF = new WeightedQuickUnionUF(N * N);
         this.grid = new boolean[N][N];
         for (int i = 0; i < N; i++) {
@@ -22,7 +22,19 @@ public class Percolation implements PercolationInterface{
     }
 
     private int xyTo1D(int row, int col) {
-        return row + col * grid.length;
+        return row + (grid.length - col - 1) * grid.length;
+    }
+
+    public int[] oneDtoXY(int num) {
+        int total = grid.length * grid.length;
+        int[] xy = new int[2];
+        if (num < 0 || num > total) {
+            xy[0] = -1;
+            xy[1] = -1;
+        }
+        xy[0] = num % grid.length;
+        xy[1] = num / grid.length;
+        return xy;
     }
 
     private boolean isValid(int row, int col) {
@@ -30,7 +42,7 @@ public class Percolation implements PercolationInterface{
             throw new IllegalArgumentException();
         }
         if (row > grid.length || col > grid.length) {
-            throw  new IndexOutOfBoundsException();
+            throw new IndexOutOfBoundsException();
         }
         return true;
     }
@@ -41,12 +53,31 @@ public class Percolation implements PercolationInterface{
             grid[row][col] = true;
             openSiteNum += 1;
         }
+        Position thePosition = new Position(row, col);
+        Direction direction = new Direction(thePosition);
+
+        if (!(col == 4) && isOpen(direction.top.x, direction.top.y)){
+            weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(direction.top.x, direction.top.y));
+        }
+        if (!(col == 0) && isOpen(direction.bottom.x, direction.bottom.y)){
+            weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(direction.bottom.x, direction.bottom.y));
+        }
+        if (!(row == 0) && isOpen(direction.left.x, direction.left.y)){
+            weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(direction.left.x, direction.left.y));
+        }
+        if (!(row == 4) && isOpen(direction.right.x, direction.right.y)){
+            weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(direction.right.x, direction.right.y));
+        }
+
     }
+
+
 
     @Override
     public boolean isOpen(int row, int col) {
         return grid[row][col];
     }
+
 
     @Override
     public boolean isFull(int row, int col) {
@@ -61,6 +92,18 @@ public class Percolation implements PercolationInterface{
     @Override
     public boolean percolates() {
         return weightedQuickUnionUF.connected(topParent, bottomParent);
+    }
+
+    public static void main(String[] args) {
+        Percolation percolation = new Percolation(5);
+        percolation.open(3, 3);
+        System.out.println(percolation.xyTo1D(3, 3));
+        percolation.open(3, 2);
+        System.out.println(percolation.xyTo1D(3, 2));
+        System.out.println(percolation.weightedQuickUnionUF.connected(8, 13));
+        percolation.open(4,4);
+        percolation.open(3, 4);
+        
     }
 
 
