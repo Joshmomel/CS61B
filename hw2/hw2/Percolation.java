5405b31dcc8a0102c5d3e7f500e56549395bba39
Bok <heybokk@gmail.com>
2018-12-31 15:14:44 +1100

    my new edit

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,159 +1,107 @@
 package hw2;
 
-
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-//import java.lang.IllegalArgumentException;
 
 public class Percolation {
-    private int N;
-    private int botTank;
-    private int edge;
-    private int topTank = 0;
-    private int openSpaces;
-    private boolean[][] tank;
-    private WeightedQuickUnionUF unionSet;
-    private WeightedQuickUnionUF unionSetNoBot;
+    private boolean[][] grid;
+    private int topParent;
+    private int bottomParent;
+    private int topForCheck;
+    private WeightedQuickUnionUF weightedQuickUnionUF;
+    private WeightedQuickUnionUF topWeightedQUF;
+    private int openSiteNum = 0;
 
-    // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
-        if (N <= 0) {
-            throw new IllegalArgumentException("Parameter cannot be a number <= 0");
-        }
+        weightedQuickUnionUF = new WeightedQuickUnionUF(N * N + 2);
+        topWeightedQUF = new WeightedQuickUnionUF(N * N + 1);
+        this.topForCheck = N * N;
+        this.topParent = N * N;
+        this.bottomParent = N * N + 1;
 
-        this.N = N;
-        botTank = (N * N) + 1;
-        edge = (N * N);
-        openSpaces = 0;
-
-        tank = new boolean[N][N];
-        for (int i = 0; i < tank.length; i++) {
-            for (int j = 0; j < tank[i].length; j++) {
-                tank[i][j] = false;
+        this.grid = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = false;
             }
+            weightedQuickUnionUF.union(xyTo1D(i, 0), bottomParent);
+            weightedQuickUnionUF.union(xyTo1D(i, N - 1), topParent);
+            topWeightedQUF.union(xyTo1D(i, N - 1), topForCheck);
         }
-
-        unionSet = new WeightedQuickUnionUF(botTank + 1);
-        unionSetNoBot = new WeightedQuickUnionUF(botTank + 1);
     }
 
-    private int getCoord(int row, int col) {
-        return (row * N) + col + 1;
+    private int xyTo1D(int row, int col) {
+        return row + (grid.length - col - 1) * grid.length;
     }
 
-    // open the site (row, col) if it is not open already
+    private int[] oneDtoXY(int num) {
+        int total = grid.length * grid.length;
+        int[] xy = new int[2];
+        if (num < 0 || num > total) {
+            xy[0] = -1;
+            xy[1] = -1;
+        }
+        xy[0] = num % grid.length;
+        xy[1] = grid.length - num / grid.length - 1;
+        return xy;
+    }
+
+    private boolean isValid(int row, int col) {
+        if (row < 0 || col < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (row > grid.length || col > grid.length) {
+            throw new IndexOutOfBoundsException();
+        }
+        return true;
+    }
+
     public void open(int row, int col) {
-        if (row < 0 || row > N - 1 || col < 0 || col > N - 1) {
-            throw new IndexOutOfBoundsException("Cannot use these values for row and column");
-        } else if (tank[row][col]) {
-            return;
+        if (isValid(row, col)) {
+            grid[row][col] = true;
+            openSiteNum += 1;
+        }
+        Position thePosition = new Position(row, col);
+        Direction direction = new Direction(thePosition);
+
+        if (!(col == grid.length - 1) && isOpen(direction.top.x, direction.top.y)){
+            weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(direction.top.x, direction.top.y));
+            topWeightedQUF.union(xyTo1D(row, col), xyTo1D(direction.top.x, direction.top.y));
+        }
+        if (!(col == 0) && isOpen(direction.bottom.x, direction.bottom.y)){
+            weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(direction.bottom.x, direction.bottom.y));
+            topWeightedQUF.union(xyTo1D(row, col), xyTo1D(direction.bottom.x, direction.bottom.y));
+        }
+        if (!(row == 0) && isOpen(direction.left.x, direction.left.y)){
+            weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(direction.left.x, direction.left.y));
+            topWeightedQUF.union(xyTo1D(row, col), xyTo1D(direction.left.x, direction.left.y));
+        }
+        if (!(row == grid.length - 1) && isOpen(direction.right.x, direction.right.y)){
+            weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(direction.right.x, direction.right.y));
+            topWeightedQUF.union(xyTo1D(row, col), xyTo1D(direction.right.x, direction.right.y));
         }
 
-        tank[row][col] = true;
-
-
-        int coord = getCoord(row, col);
-        //System.out.println(coord);
-        if (checkTop(row, col)) {
-            unionSet.union(topTank, coord);
-            unionSetNoBot.union(topTank, coord);
-        }
-
-        unionTop(row, col);
-        unionBottom(row, col);
-        unionLeft(row, col);
-        unionRight(row, col);
-
-        if (checkBot(row, col)) {
-            unionSet.union(botTank, coord);
-        }
-
-        openSpaces++;
     }
 
-    private void unionTop(int row, int col) {
-        try {
-            if (isOpen(row - 1, col)) {
-                unionSet.union(getCoord(row - 1, col), getCoord(row, col));
-                unionSetNoBot.union(getCoord(row - 1, col), getCoord(row, col));
-            }
-        } catch (IndexOutOfBoundsException ex) {
-            return;
-        }
-    }
-
-    private void unionBottom(int row, int col) {
-        try {
-            if (isOpen(row + 1, col)) {
-                unionSet.union(getCoord(row + 1, col), getCoord(row, col));
-                unionSetNoBot.union(getCoord(row + 1, col), getCoord(row, col));
-            }
-        } catch (IndexOutOfBoundsException ex) {
-            return;
-        }
-    }
-
-    private void unionLeft(int row, int col) {
-        try {
-            if (isOpen(row, col - 1)) {
-                unionSet.union(getCoord(row, col - 1), getCoord(row, col));
-                unionSetNoBot.union(getCoord(row, col - 1), getCoord(row, col));
-            }
-        } catch (IndexOutOfBoundsException ex) {
-            return;
-        }
-    }
-
-    private void unionRight(int row, int col) {
-        try {
-            if (isOpen(row, col + 1)) {
-                unionSet.union(getCoord(row, col + 1), getCoord(row, col));
-                unionSetNoBot.union(getCoord(row, col + 1), getCoord(row, col));
-            }
-        } catch (IndexOutOfBoundsException ex) {
-            return;
-        }
-    }
-
-    // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        //System.out.println(N - 1);
-        if (row < 0 || row > (N - 1) || col < 0 || col > (N - 1)) {
-            throw new IndexOutOfBoundsException("Cannot use these values for row and column");
-        }
-
-        return tank[row][col];
+        return grid[row][col];
     }
 
-    private boolean checkTop(int row, int col) {
-        return getCoord(row, col) <= N;
-    }
 
-    private boolean checkBot(int row, int col) {
-        int coord = getCoord(row, col);
-        return coord >= (botTank - N) && coord <= edge;
-    }
-
-    // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        if (row < 0 || row > N - 1 || col < 0 || col > N - 1) {
-            throw new IndexOutOfBoundsException("Cannot use these values for row and column");
+        if (percolates()) {
+            return topWeightedQUF.connected(xyTo1D(row, col), topForCheck) && isOpen(row, col);
         }
-
-        int coord = getCoord(row, col);
-        return unionSetNoBot.connected(topTank, coord) && isOpen(row, col);
+        return weightedQuickUnionUF.connected(xyTo1D(row, col), topParent) && isOpen(row, col);
     }
 
-    // number of open sites
     public int numberOfOpenSites() {
-        return openSpaces;
+        return openSiteNum;
     }
 
-    // does the system percolate?
     public boolean percolates() {
-        return unionSet.connected(topTank, botTank);
+        return weightedQuickUnionUF.connected(topParent, bottomParent);
     }
 
-    // use for unit testing (not required)
     public static void main(String[] args) {
         Percolation test = new Percolation(3);
         test.open(0, 2);
@@ -162,5 +110,44 @@ public class Percolation {
         test.open(2, 0);
         System.out.println(test.percolates());
         System.out.println(test.isFull(2, 0));
+
+//        Percolation percolation = new Percolation(5);
+//
+//        percolation.open(percolation.oneDtoXY(12)[0], percolation.oneDtoXY(12)[1]);
+//        System.out.println("12: " +percolation.oneDtoXY(12)[0] + " , " + percolation.oneDtoXY(12)[1]);
+//        percolation.open(percolation.oneDtoXY(14)[0], percolation.oneDtoXY(14)[1]);
+//        System.out.println("14: " +percolation.oneDtoXY(14)[0] + " , " + percolation.oneDtoXY(14)[1]);
+//        percolation.open(percolation.oneDtoXY(19)[0], percolation.oneDtoXY(19)[1]);
+//        System.out.println("19: " + percolation.oneDtoXY(19)[0] + " , " + percolation.oneDtoXY(19)[1]);
+////        System.out.println(percolation.weightedQuickUnionUF.connected(19, 12));
+//        percolation.open(percolation.oneDtoXY(13)[0], percolation.oneDtoXY(13)[1]);
+//        System.out.println("13: " +percolation.oneDtoXY(13)[0] + " , " + percolation.oneDtoXY(13)[1]);
+////        System.out.println(percolation.weightedQuickUnionUF.connected(19, 12));
+//
+//        percolation.open(percolation.oneDtoXY(2)[0], percolation.oneDtoXY(2)[1]);
+//        System.out.println("2: " +percolation.oneDtoXY(2)[0] + " , " + percolation.oneDtoXY(2)[1]);
+//        percolation.open(percolation.oneDtoXY(7)[0], percolation.oneDtoXY(7)[1]);
+//        System.out.println("7: " +percolation.oneDtoXY(7)[0] + " , " + percolation.oneDtoXY(7)[1]);
+////        System.out.println("isOpen(4 ,0): " + percolation.isOpen(4 ,0));
+//        System.out.println("24: " +percolation.oneDtoXY(24)[0] + " , " + percolation.oneDtoXY(24)[1]);
+//
+//
+////        System.out.println("isFull(4, 0): " + percolation.xyTo1D(4,0) + " : "  + percolation.isFull(4, 0));
+//        System.out.println(percolation.percolates());
+//        percolation.open(4, 0);
+//        System.out.println(percolation.percolates());
+//        System.out.println("-------------------------");
+//
+//        percolation.open(percolation.oneDtoXY(16)[0], percolation.oneDtoXY(16)[1]);
+//        percolation.open(percolation.oneDtoXY(21)[0], percolation.oneDtoXY(21)[1]);
+//        percolation.open(percolation.oneDtoXY(22)[0], percolation.oneDtoXY(22)[1]);
+//        percolation.open(percolation.oneDtoXY(20)[0], percolation.oneDtoXY(20)[1]);
+//
+////        System.out.println("connect 16 : " + percolation.weightedQuickUnionUF.connected(16, 21));
+//        System.out.println(percolation.isFull(percolation.oneDtoXY(21)[0], percolation.oneDtoXY(21)[1]));
+//
+
     }
+
+
 }
