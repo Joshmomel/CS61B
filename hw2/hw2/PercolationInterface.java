530cd7d6698be57bb39afa5a21aaf59866c2f2f2
Bok <heybokk@gmail.com>
2018-12-31 12:17:51 +1100

    delete interface

--- hw2/hw2/PercolationInterface.java
+++
@@ -1,9 +0,0 @@
-package hw2;
-
-public interface PercolationInterface {
-    public void open(int row, int col);       // open the site (row, col) if it is not open already
-    public boolean isOpen(int row, int col);  // is the site (row, col) open?
-    public boolean isFull(int row, int col);  // is the site (row, col) full?
-    public int numberOfOpenSites();           // number of open sites
-    public boolean percolates();              // does the system percolate?
-}
