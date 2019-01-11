df490039cc59db9a9bc5318e554ae76df263ce75
Bok <heybokk@gmail.com>
2019-01-12 10:23:02 +1100

    Raster done with print statement

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -8,9 +8,14 @@ import java.util.Map;
  * not draw the output correctly.
  */
 public class Rasterer {
+    private final double Sl = 288200;
+    private final double startPixel = ((MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / MapServer.TILE_SIZE) * Sl;
+    private final double difference = MapServer.ROOT_LRLON - MapServer.ROOT_ULLON;
+    private final double latDifference = MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT;
+
+
 
     public Rasterer() {
-        // YOUR CODE HERE
     }
 
     /**
@@ -42,10 +47,154 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
+        Double queryLrlon = params.get("lrlon");
+        Double queryUllon = params.get("ullon");
+        Double queryUllat = params.get("ullat");
+        Double queryLrlat = params.get("lrlat");
+        Double queryWidth = params.get("w");
         System.out.println(params);
+
+        double LonDPP = calculateLonDDP(queryLrlon, queryUllon, queryWidth);
+        System.out.println(LonDPP);
+        int depth = calculateDepth(LonDPP);
+
+        double[] longitude = getLongitude(depth, queryUllon, queryLrlon);
+        System.out.println("longitude are " + longitude[0] + ", " + longitude[1] + " and " + longitude[2] + " , " + longitude[3]);
+
+        double[] latitude = getLatitude(depth, queryLrlat, queryUllat);
+        System.out.println("latitude are " + latitude[0] + ", " + latitude[1] + " and " + latitude[2] + " , " + latitude[3]);
+
+        String[][] renderGrid = renderGrid(depth, (int) longitude[1], (int) longitude[3], (int) latitude[1], (int) latitude[3]);
+        for (String[] strings : renderGrid) {
+            for (String string : strings) {
+                System.out.println(string);
+            }
+        }
+
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+
+
+        results.put("raster_ul_lon", longitude[0]);
+        results.put("raster_ul_lat", latitude[0]);
+        results.put("raster_lr_lon", longitude[2]);
+        results.put("raster_lr_lat", latitude[1]);
+        results.put("depth", depth);
+        results.put("query_success", true);
+        results.put("render_grid", renderGrid);
+
+
+        return results;
+    }
+
+    private double calculateLonDDP(double lrlon, double ullon, double width) {
+        double diff = lrlon - ullon;
+        System.out.println("system diff is " + (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON));
+        System.out.println("x0 per pixel " + startPixel);
+        System.out.println("diff is " + diff);
+        System.out.println("width is " + width);
+        System.out.println("initial pdd is " + diff * Sl);
+        System.out.println("after scaling is " + diff * Sl / width);
+        double LonDpp = diff / width;
+        System.out.println("Approx LonDpp is " + LonDpp);
+        double feetPerPixel = LonDpp * Sl;
+        System.out.println("depth is " + calculateDepth(feetPerPixel));
+        return feetPerPixel;
+    }
+
+    private int calculateDepth(double feetPerpixel) {
+        int count = 1;
+        double initPixel = startPixel / 2;
+        for (int i = 0; i < 8; i++) {
+            if (feetPerpixel > initPixel) {
+                return count;
+            }
+            initPixel = initPixel / 2;
+            count += 1;
+        }
+        return 7;
+    }
+
+    private double[] getLatitude(int depth, double requireMinLat, double requireMaxLat) {
+        double differenceInDepth = latDifference / Math.pow(2, depth);
+//        System.out.println("min is " + requireMinLat);
+//        System.out.println("max is " + requireMaxLat);
+        int countMin = 0;
+        double[] results = new double[4];
+        double lat = MapServer.ROOT_ULLAT;
+
+        for (int i = 0; i < Math.pow(2, depth); i++) {
+            if (requireMaxLat > lat - differenceInDepth) {
+                results[0] = lat;
+                results[1] = countMin;
+                break;
+            }
+            lat -= differenceInDepth;
+            countMin += 1;
+        }
+
+        for (int i = countMin; i <= Math.pow(2, depth); i++) {
+            if (i == Math.pow(2,depth)) {
+                results[2] = MapServer.ROOT_LRLAT;
+                results[3] = i - 1;
+                break;
+            }
+            if (requireMinLat > lat - differenceInDepth) {
+                results[2] = lat - differenceInDepth;
+                results[3] = i;
+                break;
+            }
+            lat -= differenceInDepth;
+        }
+        return results;
+    }
+
+    private double[] getLongitude(int depth, double requireMinLon, double requireMaxLon) {
+        double differenceInDepth = difference / Math.pow(2, depth);
+        System.out.println("function getLongitude diff is " + differenceInDepth);
+        int countMin = 0;
+        double[] results = new double[4];
+        double lon = MapServer.ROOT_ULLON;
+
+        for (int i = 0; i < Math.pow(2, depth); i++) {
+            if (requireMinLon < lon + differenceInDepth) {
+                results[0] = lon;
+                results[1] = countMin;
+                break;
+            }
+            lon += differenceInDepth;
+            countMin += 1;
+        }
+
+        for (int i = countMin; i <= Math.pow(2, depth); i++) {
+            if (i == Math.pow(2, depth)) {
+                results[2] = MapServer.ROOT_LRLON;
+                results[3] = i - 1;
+                break;
+            }
+            if (requireMaxLon < lon + differenceInDepth) {
+                results[2] = lon + differenceInDepth;
+                results[3] = i;
+                break;
+            }
+            lon += differenceInDepth;
+        }
+        return results;
+    }
+
+    private String[][] renderGrid(int depth, int lonStart, int lonEnd, int latStart, int latEnd) {
+        String[][] results = new String[latEnd - latStart + 1][lonEnd - lonStart + 1];
+        for (int i = latStart; i <= latEnd; i++) {
+            for (int j = lonStart; j <= lonEnd; j++) {
+                if (i - latStart < 0 || j - lonStart < 0) {
+                    System.out.println("break happens in renderGrid");
+                    break;
+                } else {
+                    results[i - latStart][j - lonStart]
+                            = "d" + depth + "_x" + j + "_y" + i + ".png";
+                }
+
+            }
+        }
         return results;
     }
 
